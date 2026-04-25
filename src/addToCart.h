f073fd94559087efff7b3bae111d4ec4bec9d86f
ag101 <amrashraf20210@gmail.com>
2026-04-26 00:51:29 +0300

    rewrite addToCart

--- src/addToCart.h
+++ src/addToCart.h
@@ -1,13 +1,17 @@
+#pragma once
+#include "Product.h"
 #include "inventory.h"
+#include <vector>
 class AddToCart {
-  Inventory inv;
-  Inventory cart;
-
 public:
-  void execute(unsigned id, unsigned quantity) {
-    if (inv.getProduct(id)->getQuantity() >= quantity) {
-      cart.addProduct(*inv.getProduct(id));
-      cart.addStock(id, quantity);
+  static void execute(int id, int quantity, Inventory &inv,
+                      std::vector<Product> &cart) {
+    Product *product = inv.getProduct(id);
+
+    if (product != nullptr && product->getQuantity() >= quantity) {
+      cart.push_back(*product);
+
+      inv.removeStock(id, quantity);
     }
   }
 };
